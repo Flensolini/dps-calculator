d17146270f9c2eb7698bce12793273f7438f5c64
Flensolini <k.myhre@live.no>
2020-07-01 13:56:08 +0200

    Updates

---
+++ precache-manifest.0a919c9e3cf20671b8fe1df58ea60bd8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17842824d3e62ae6739b0ce9cd63cd78",
+    "url": "/dps-calculator/index.html"
+  },
+  {
+    "revision": "2539ef3ffd05568114be",
+    "url": "/dps-calculator/static/css/main.8f1f4d4a.chunk.css"
+  },
+  {
+    "revision": "ecf066056d153f32660b",
+    "url": "/dps-calculator/static/js/2.8008c35a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dps-calculator/static/js/2.8008c35a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2539ef3ffd05568114be",
+    "url": "/dps-calculator/static/js/main.db52e4e4.chunk.js"
+  },
+  {
+    "revision": "1ecb12950314bb0e9b6a",
+    "url": "/dps-calculator/static/js/runtime-main.f8b9e363.js"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/dps-calculator/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/dps-calculator/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/dps-calculator/static/media/OpenSans-Regular.3ed9575d.ttf"
+  }
+]);
